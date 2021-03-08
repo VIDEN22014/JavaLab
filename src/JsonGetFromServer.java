caf12f451d5afdfb900b8418eba08dbd91427bb5
Bodgan Humenyk <dxbogdan4@gmail.com>
2021-03-08 18:33:44 +0200

    Lab 3 City List Done!

--- src/JsonGetFromServer.java
+++ src/JsonGetFromServer.java
@@ -3,19 +3,19 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class JsonGet {
+public class JsonGetFromServer {
 
     private static final String USER_AGENT = "Mozilla/5.0";
 
-    private static final String GET_URL = "http://api.openweathermap.org/data/2.5/forecast?lat=48.9215&lon=24.7097&APPID=f75c35058cf73360f1ae084bd6097aa5";
 
-    public static void sendGET() throws IOException {
+
+    public static boolean sendGET(String cityName) throws IOException {
+        String GET_URL = "http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&APPID=f75c35058cf73360f1ae084bd6097aa5";
         URL obj = new URL(GET_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
         int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
         if (responseCode == HttpURLConnection.HTTP_OK) { // success
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     con.getInputStream()));
@@ -27,12 +27,13 @@ public class JsonGet {
             }
             in.close();
 
-            FileWriter file = new FileWriter("C://Users/dxbog/IdeaProjects/JavaLab/src/json.json");
+            FileWriter file = new FileWriter("C://Users/dxbog/IdeaProjects/JavaLab/src/Forecast.json");
             file.write(response.toString());
             file.flush();
             file.close();
+            return true;
         } else {
-            System.out.println("GET request not worked");
+            return false;
         }
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.data.Data;
-import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        JsonGet jsonGet = new JsonGet();
-        jsonGet.sendGET();
+        JsonGetFromServer jsonGetFromServer = new JsonGetFromServer();
+        java.util.List<String> cityNames = new ArrayList<String>();
+        File file = new File("C://Users/dxbog/IdeaProjects/JavaLab/src/CityNames.txt");
+        FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
+        String line = reader.readLine();
+        while (line != null) {
+            cityNames.add(line);
+            line = reader.readLine();
+        }
         JSON json = new JSON();
-        Data data = json.deserializeFromJsonFile("C://Users/dxbog/IdeaProjects/JavaLab/src/json.json");
-        for (com.data.List i : data.list) {
-            System.out.println(i.dtTxt + " Температура: " + (int) (i.main.temp - 273.15) + "°C");
+        Data data = json.deserializeFromJsonFile("C://Users/dxbog/IdeaProjects/JavaLab/src/Forecast.json");
+        for (String cityName : cityNames) {
+            if (jsonGetFromServer.sendGET(cityName)) {
+                data = json.deserializeFromJsonFile("C://Users/dxbog/IdeaProjects/JavaLab/src/Forecast.json");
+                System.out.println("Прогноз температури для міста : " + cityName);
+                for (com.data.List i : data.list) {
+                    System.out.println(i.dtTxt + " Температура: " + (int) (i.main.temp - 273.15) + "°C");
+                }
+            } else {
+                System.out.println("Введіть правильну назву міста");
+            }
+            System.out.println("");
         }
     }
 }
