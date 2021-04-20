ee292dbd7c8fd102395291b524c464e1a59c4a31
VIDEN22014 <dxbogdan4@gmail.com>
2021-04-20 10:00:42 +0300

    Lab 2 Done

--- src/Forest.java
+++ src/Forest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Forest {
-    List<Bird> birdList = new ArrayList<Bird>();
+    List<Bird> birdList = new ArrayList<>();
     FactoryProducer factoryProducer = new FactoryProducer();
 
     void create() {
