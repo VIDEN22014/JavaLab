ad625f00d850bb36de8667dd839c5d7610cca813
VIDEN22014 <dxbogdan4@gmail.com>
2021-04-20 12:36:54 +0300

    Lab 3 Done

--- src/Birds/Bird.java
+++ src/Birds/Bird.java
@@ -1,19 +1,22 @@
 package Birds;
 
 public abstract class Bird {
-    boolean isUp=false;
-    public void up(){
-        System.out.println(this.getClass()+" is up");
-        isUp=true;
+    boolean isUp = false;
+
+    public void up() {
+        System.out.println(this.getClass() + " is up");
+        isUp = true;
     }
-    public void down(){
-        System.out.println(this.getClass()+" is down");
-        isUp=false;
+
+    public void down() {
+        System.out.println(this.getClass() + " is down");
+        isUp = false;
     }
-    public void danger(){
-        if (isUp){
-            System.out.println(this.getClass()+" is hide");
-            isUp=false;
+
+    public void danger() {
+        if (isUp) {
+            System.out.println(this.getClass() + " is hide");
+            isUp = false;
         }
     }
 }
