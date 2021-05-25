aab24a5d62d0d6d41e68308cc90480a45bfd1dd1
VIDEN <dxbohdan4@gmail.com>
2021-05-25 14:22:12 +0300

    Lab8 ThreadsParallel Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-import java.io.BufferedWriter;
-import java.io.Writer;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         long threadTime;
+        StringBuilder csvStringBuilder = new StringBuilder("ThreadCount;Time\n");
+        double tempArea;
+        double tempTime;
 
-        for (int i = 1; i <= 100; i++) {
-            threadTime = System.currentTimeMillis();
-            System.out.println("Кількість потоків : " + i);
+        for (int i = 1; i <= 250; i++) {
             AreaAdder adder=new AreaAdder();
             ThreadController threadController = new ThreadController();
-            System.out.println("Площа = " + threadController.ParallelIntegrate(i, 0, Math.PI / 2.0, 0.0000001,adder));
-            System.out.println("Час виконання : " + (double) (System.currentTimeMillis() - threadTime) + "\n");
+            threadTime = System.currentTimeMillis();
+            tempArea=threadController.ParallelIntegrate(i, 0, Math.PI / 2.0, 0.0000001,adder);
+            tempTime=(double) (System.currentTimeMillis() - threadTime);
+            //Вивід даних
+            System.out.println("Кількість потоків : " + i);
+            System.out.println("Площа = " + tempArea);
+            System.out.println("Час виконання : " + (int)tempTime + "\n");
+            csvStringBuilder.append(i);
+            csvStringBuilder.append(";");
+            csvStringBuilder.append((int)tempTime);
+            csvStringBuilder.append("\n");
         }
+        //Створення файлу csv
+        FileWriter fileWriter = new FileWriter("data.csv");
+        fileWriter.write(csvStringBuilder.toString());
+        fileWriter.close();
+
     }
 }
